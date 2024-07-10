6c33044f00b2a3297fc6879995c8e693fd512b73
saajand <saajanduwal2000@gmail.com>
2024-07-10 11:19:31 +0545

    Add Rainbowkit and Wagmi packages for EVM wallet connections

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import CosmosWalletWidget from './components/CosmosWalletWidget';
 import { CosmosChains } from './constants/chains';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { sepolia, baseSepolia } from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+import EVMManagerPage from './pages/EVMManagerPage';
+
+const WALLET_CONNECT_PROJECT_ID = import.meta.env.VITE_APP_WALLET_CONNECT_PROJECT_ID;
 
 function App() {
+  const queryClient = new QueryClient();
+
+  const config = getDefaultConfig({
+    appName: 'IBC',
+    projectId: WALLET_CONNECT_PROJECT_ID,
+    chains: [sepolia, baseSepolia],
+    ssr: false,
+  });
+
   return (
     <>
-      <div className="app">
-        <h3 className="text-lg font-bold">IBC</h3>
-        <div className="w-[400px]">
-          <CosmosWalletWidget chain={CosmosChains.archway} />
-        </div>
-      </div>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <div className="app">
+              <h3 className="text-lg font-bold">IBC</h3>
+
+              <div>
+                <div className="w-[400px]">
+                  <CosmosWalletWidget chain={CosmosChains.archway} />
+                </div>
+
+                <EVMManagerPage />
+              </div>
+            </div>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </>
   );
 }
---
+++ src/pages/EVMManagerPage.tsx
@@ -0,0 +1,13 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+const EVMManagerPage = () => {
+  return (
+    <div className="evm-manager-page">
+      <div>
+        <ConnectButton />
+      </div>
+    </div>
+  );
+};
+
+export default EVMManagerPage;
